23ff87aaa1a093af43ef4566f0b336dd55bfa7bc
Sercobos-Col <sercobos@gmail.com>
2025-07-22 15:58:11 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Instalar Chromium
 apt-get update -y
 apt-get install -y wget gnupg2 curl unzip
 wget -q -O - https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb
 dpkg -i /tmp/chrome.deb
 apt-get -y install -f
+
+# Instalar Playwright
 playwright install
