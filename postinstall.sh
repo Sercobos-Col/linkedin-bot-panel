3d9612aa36731d79c47e7a6e93c3d3d48261f84e
Sercobos-Col <sercobos@gmail.com>
2025-07-22 15:47:32 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-playwright install chromium
+apt-get update -y
+apt-get install -y wget gnupg2 curl unzip
+wget -q -O - https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb
+dpkg -i /tmp/chrome.deb
+apt-get -y install -f
+playwright install
